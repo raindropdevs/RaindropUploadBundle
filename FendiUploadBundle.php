5c8ca2f97a2231bf36a4f82df4f5ec5512db0bfd
Enrico Antonello <eantonello@h-art.com>
2014-01-27 10:52:21 +0100

    first commit

---
+++ FendiUploadBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Raindrop\UploadBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RaindropUploadBundle extends Bundle
+{
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-RaindropUploadAssetsBundle
+RaindropUploadBundle
 ==========================
